476e271c760d567fee3aedc4c89ec16dc83269ec
fullStackOasis <fullstackdev@fullstackoasis.com>
2020-09-01 19:14:41 -0400

    Addresses issue #1 Handler keeps running even though Activity is destroyed When onStop is called, use Handler's removeCallbacksAndMessages to stop the timer. This will stop the repeated calling of showNextImage.

--- app/src/main/java/com/fullstackoasis/revolvingimagesscreensaver/MainActivity.java
+++ app/src/main/java/com/fullstackoasis/revolvingimagesscreensaver/MainActivity.java
@@ -18,6 +18,11 @@ public class MainActivity extends AppCompatActivity {
     private static int IMAGE_NUM = 0;
     private static int[] images = {R.drawable.a_lost_creek_lake, R.drawable.b_bamiyan_afghanistan,
         R.drawable.c_gakona_alaska, R.drawable.d_willamette_falls };
+    private boolean stopped = false;
+
+    /**********************************************************************************************
+     * Lifecycle methods
+     **********************************************************************************************/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +32,47 @@ public class MainActivity extends AppCompatActivity {
         setTimer();
     }
 
+    /**
+     * If the Activity starts, let revolving images continue.
+     */
+    @Override
+    protected void onStart() {
+        super.onStart();
+        stopped = false;
+        Log.d(TAG, "Finished with onStart method");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        stopped = false;
+        Log.d(TAG, "Finished with onResume method");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "Finished with onPause method");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "Finished with onStop method");
+        // Activity is no longer visible. Stop the Handler, and
+        waitHandler.removeCallbacksAndMessages(null);
+        waitHandler = null;
+        stopped = true;
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "Finished with onDestroy method");
+    }
+
     private void setTimer() {
+        if (stopped) return;
         if (waitHandler != null) {
             Log.d(TAG, "setTimer waitHandler was not null");
             // See https://stackoverflow.com/questions/22718951/stop-handler-postdelayed/22719065
@@ -41,6 +86,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     protected void showNextImage() {
+        if (stopped) return;
         Log.d(TAG, "showNextImage going to setTimer");
         ImageView iv = this.findViewById(R.id.imageView);
         iv.setImageDrawable(getNextImage());
@@ -59,7 +105,10 @@ public class MainActivity extends AppCompatActivity {
 
     class ImageTracker implements Runnable {
         public void run() {
-            Log.d(TAG, "run!"); MainActivity.this.showNextImage();
+            if (!stopped) {
+                Log.d(TAG, "run!");
+                MainActivity.this.showNextImage();
+            }
         }
     }
 
